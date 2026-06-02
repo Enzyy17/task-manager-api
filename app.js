76a75c1b9e241d6bf49db3282e33c55479f19c1f
Enzyy <enzyygreat@gmail.com>
2026-06-02 15:36:02 +0100

    made changes to app.js, dotenv and package.json

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import connectDB from './src/config/db.js';   // your DB connection function
+
+const PORT= process.env.PORT || 6000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+
+console.log("welcome to task manager api")
+res.status(200).json})
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`)
+})
+
+connectDB();  
+
+
+
+
--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,8 @@
 // src/config/db.js
 import mongoose from 'mongoose';
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
